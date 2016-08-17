1485337f8df0d71152aa52681d4ff3426b3197bb
avinashmishra <avinash@infosoftjoin.in>
2016-08-17 17:29:09 +0530

    second commit

---
+++ QuicKonnect/QuicKonnect/QKForgotVC.h
@@ -0,0 +1,13 @@
+//
+//  QKForgotVC.h
+//  QuicKonnect
+//
+//  Created by Vijayalakshmi Shankar on 8/17/16.
+//  Copyright © 2016 Amit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QKForgotVC : UIViewController
+
+@end
